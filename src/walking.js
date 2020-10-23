33dfcbd605558049394d692acdb431ff41911334
Frederic Junod <frederic.junod@camptocamp.com>
2020-10-23 14:10:20 +0200

    Adapt formula based on wandern corrections

--- src/walking.js
+++ src/walking.js
@@ -1,4 +1,4 @@
-// Publicly avaiable formula from: https://www.randonner.ch/fr/signalisation/signalisation/temps-de-marche
+// Publicly available formula from: https://www.randonner.ch/fr/signalisation/signalisation/temps-de-marche
 
 const CS = [
   14.271,
@@ -20,6 +20,16 @@ const CS = [
 ];
 
 /**
+ * @param {number} num
+ * @param {number} min
+ * @param {number} max
+ * @return {number}
+ */
+function clamp(num, min, max) {
+  return num <= min ? min : num >= max ? max : num;
+}
+
+/**
  * Calculates the walking time of a segment.
  * @param {number} length Length in meters.
  * @param {number} heightDifference Height difference in meters.
@@ -29,16 +39,10 @@ export function computeWalkingTime(length, heightDifference) {
   if (length === 0) {
     return 0;
   }
-  const slope10 = 10 * heightDifference / length;
+  const slope10 = clamp(10 * heightDifference / length, -4, 4);
   let minPerKm = 0; // walking speed in minutes per kilometer
-  if (-4 < slope10 && slope10 < 4) {
-    for (let i = 0, ii = CS.length; i < ii; i++) {
-      minPerKm += CS[i] * Math.pow(slope10, i);
-    }
-  } else if (slope10 > 0) {
-    minPerKm = 17 * slope10;
-  } else {
-    minPerKm = -9 * slope10;
+  for (let i = 0, ii = CS.length; i < ii; i++) {
+    minPerKm += CS[i] * Math.pow(slope10, i);
   }
   const secondsPerMeter = 60 * minPerKm / 1000;
   return secondsPerMeter * length;
