e349fa1ed7632729c8a0139c4b8bf111dd9dee7b
Frederic Junod <frederic.junod@camptocamp.com>
2021-01-06 16:35:36 +0100

    Add more tests

--- test/walking.test.js
+++ test/walking.test.js
@@ -7,9 +7,26 @@ describe('tracktime', () => {
 
   describe('computeWalkingTime', () => {
     it('should return 0 if length is <= 0', () => {
-      expect(computeWalkingTime(0, 42)).to.eql(0);
-      expect(computeWalkingTime(-10, 9)).to.eql(0);
+      expect(computeWalkingTime(0, 42)).to.be.eql(0);
+      expect(computeWalkingTime(-10, 9)).to.be.eql(0);
     });
+
+    it('slope > 40% is clamped', () => {
+      const below40 = computeWalkingTime(100, 30);
+      const max = computeWalkingTime(100, 40);
+      const above40 = computeWalkingTime(100, 50);
+      expect(below40).to.be.below(max);
+      expect(above40).to.be.eql(max);
+    });
+
+    it('slope < 40% is clamped', () => {
+      const above40 = computeWalkingTime(100, -30);
+      const max = computeWalkingTime(100, -40);
+      const below40 = computeWalkingTime(100, -50);
+      expect(below40).to.be.eql(max);
+      expect(above40).to.be.below(max);
+    });
+
   });
 
 });
